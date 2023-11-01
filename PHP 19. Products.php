02b7aab3fd7939359ab6c7e830e2ae9b789e7e5d
Sabiq Hashil <sabiqhashilkp786@gmail.com>
2023-11-01 22:18:53 +0530

    Product

---
+++ PHP 19. Products.php
@@ -0,0 +1,48 @@
+<!--
+    19 - Aim:
+    Create a table product with fields itemcode, itemname, unitprice, Write PHP
+    program to insert 5 records into the table and display it in a table format.
+-->
+
+<html>
+    <head>
+        <title>Product</title>
+    </head>
+
+    <body>
+        <h1><center>List of Products</center></h1>
+        <form method="POST" action="">
+            Item Code: <input type="text" name="code">
+            <br> <br> <br>
+            Item Name: <input type="text" name="name">
+            <br> <br> <br>
+            Unit Price: <input type="text" name="price">
+            <br> <br> <br>
+            <input type="submit" value="Display">
+        </form>
+    </body>
+</html>
+
+<?php
+
+    if($_POST) {
+        $no = $_POST['code'];
+        $name = $_POST['name'];
+        $price = $_POST['price'];
+        $con = pg_connect("host=localhost dbname=college user=postgres password=123");
+        if($con) {
+            echo "Successfully connected!";
+            $qry = "insert into product values ('$no', '$name', '$price')";
+            $result = pg_query($con, $qry);
+            $qry1 = "select * from product";
+            $result1 = pg_query($con,$qry1);
+			echo "<table border=1>";
+			echo "<tr><th>Item Code</th><th>Item Name</th><th>Unit Price</th></tr>";
+            while($row = pg_fetch_row($result1)) {
+                echo "<br>\n";
+                echo "<tr><td>Item No: $row[0] </td><td> Item Name: $row[1] </td><td> Unit Price: $row[2] </td></tr>";
+            }
+        }
+    }
+
+?>
