5d11350dca02049daf7215f3db7b4de61741334e
Sabiq Hashil <sabiqhashilkp786@gmail.com>
2023-11-05 22:49:45 +0530

    updated

--- PHP 19. Products.php
+++ PHP 19. Products.php
@@ -3,7 +3,6 @@
     Create a table product with fields itemcode, itemname, unitprice, Write PHP
     program to insert 5 records into the table and display it in a table format.
 -->
-
 <html>
     <head>
         <title>Product</title>
@@ -29,16 +28,16 @@
         $no = $_POST['code'];
         $name = $_POST['name'];
         $price = $_POST['price'];
-        $con = pg_connect("host=localhost dbname=college user=postgres password=123");
+        $con = mysqli_connect('localhost', 'sabiq', '123', 'college');
         if($con) {
             echo "Successfully connected!";
             $qry = "insert into product values ('$no', '$name', '$price')";
-            $result = pg_query($con, $qry);
+            $result = mysqli_query($con, $qry);
             $qry1 = "select * from product";
-            $result1 = pg_query($con,$qry1);
+            $result1 = mysqli_query($con,$qry1);
 			echo "<table border=1>";
 			echo "<tr><th>Item Code</th><th>Item Name</th><th>Unit Price</th></tr>";
-            while($row = pg_fetch_row($result1)) {
+            while($row = mysqli_fetch_row($result1)) {
                 echo "<br>\n";
                 echo "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td></tr>";
             }
